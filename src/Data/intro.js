0d219ab6daee5719945b7822f57d215e0bfea63e
BobWassermann <bob@wassermann.nl>
2018-03-17 22:11:40 +0100

    GA

--- src/Data/intro.js
+++ src/Data/intro.js
@@ -25,6 +25,7 @@ export default class Intro extends Component {
           mode='fit' />
         <Article>
           <Paragraph>Op 21 maart vinden de gemeenteraadsverkiezingen weer plaats. Inwoners van de gemeente Vaals kunnen stemmen op een zestal partijen. Mijn zus, Floor, en ik hebben de partijprogramma’s voor jullie doorgenomen. Op deze microsite vergelijken we de programma’s van iedere partij, ingedeeld in de belangrijkste overeenkomende onderwerpen.</Paragraph>
+          <Paragraph>Klik op citaten die jouw voorkeur hebben. Wij houden vervolgens in de navigatie de tel bij.</Paragraph>
           <Paragraph>Foutje of misinterpretatie? Neem contact op met <a href="mailto:correcties@vaalsstemt.nl">correcties@vaalsstemt.nl</a></Paragraph>
           <Paragraph>— Bob en Floor</Paragraph>
         </Article>
