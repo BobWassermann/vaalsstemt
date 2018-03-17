a2ed4d2a8edf610c1ed34b7897373e211681e837
BobWassermann <bob@wassermann.nl>
2018-03-17 15:19:36 +0100

    Updated email address

--- src/Data/intro.js
+++ src/Data/intro.js
@@ -25,7 +25,7 @@ export default class Intro extends Component {
           mode='fit' />
         <Article>
           <Paragraph>Op 21 maart vinden de gemeenteraadsverkiezingen weer plaats. Inwoners van de gemeente Vaals kunnen stemmen op een zestal partijen. Mijn zus, Floor, en ik hebben de partijprogramma’s voor jullie doorgenomen. Op deze microsite vergelijken we de programma’s van iedere partij, ingedeeld in de belangrijkste overeenkomende onderwerpen.</Paragraph>
-          <Paragraph>Foutje of misinterpretatie? Neem contact op met <a href="mailto:oeps@vaalsstemt.nl">oeps@vaalsstemt.nl</a></Paragraph>
+          <Paragraph>Foutje of misinterpretatie? Neem contact op met <a href="mailto:correcties@vaalsstemt.nl">correcties@vaalsstemt.nl</a></Paragraph>
           <Paragraph>— Bob en Floor</Paragraph>
         </Article>
       </Fragment>
