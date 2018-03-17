175065f2c7c579f692b7303dbf194692b90bc3a6
BobWassermann <bob@wassermann.nl>
2018-03-17 15:14:23 +0100

    Alignment fix

--- src/Data/intro.js
+++ src/Data/intro.js
@@ -4,10 +4,13 @@ import styled from 'styled-components'
 const Article = styled.article`
   flex: 100% 1;
   width: 90%;
-  max-width: 550px;
   text-align: justify;
   margin-top: 50px;
   display: block;
+`
+
+const Paragraph = styled.p`
+  max-width: 550px;
 `
 
 export default class Intro extends Component {
@@ -21,9 +24,9 @@ export default class Intro extends Component {
           height={158}
           mode='fit' />
         <Article>
-          <p>Op 21 maart vinden de gemeenteraadsverkiezingen weer plaats. Inwoners van de gemeente Vaals kunnen stemmen op een zestal partijen. Mijn zus, Floor, en ik hebben de partijprogramma’s voor jullie doorgenomen. Op deze microsite vergelijken we de programma’s van iedere partij, ingedeeld in de belangrijkste overeenkomende onderwerpen.</p>
-          <p>Foutje of misinterpretatie? Neem contact op met <a href="mailto:oeps@vaalsstemt.nl">oeps@vaalsstemt.nl</a></p>
-          <p>— Bob en Floor</p>
+          <Paragraph>Op 21 maart vinden de gemeenteraadsverkiezingen weer plaats. Inwoners van de gemeente Vaals kunnen stemmen op een zestal partijen. Mijn zus, Floor, en ik hebben de partijprogramma’s voor jullie doorgenomen. Op deze microsite vergelijken we de programma’s van iedere partij, ingedeeld in de belangrijkste overeenkomende onderwerpen.</Paragraph>
+          <Paragraph>Foutje of misinterpretatie? Neem contact op met <a href="mailto:oeps@vaalsstemt.nl">oeps@vaalsstemt.nl</a></Paragraph>
+          <Paragraph>— Bob en Floor</Paragraph>
         </Article>
       </Fragment>
     )
